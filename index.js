217210ca5f2f8761bc22dc98a7bc5e3ab7cb71bb
沈晓鹏 <shu_pian@hunliji.com>
2025-03-06 20:55:42 +0800

    add ia32 and fixed function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-const binding = require('./src/addon');
+const os = require('os');
+const path = require('path');
+
+const modulePath = `./src/${os.arch()}/addon`;
+const binding = require(path.resolve(__dirname, modulePath));
 
 function initialize(appId) {
     return binding.initialize(appId);
@@ -12,13 +16,28 @@ function login(onLoginSucess, onLoginError, onLogout, token) {
     return binding.login(onLoginSucess, onLoginError, onLogout, token);
 }
 
+function logout(account) {
+    return binding.logout(account);
+}
+
 function makeCall(onCallError, onCallStateChange, onMuteStateChange, phone, payload) {
     return binding.makeCall(onCallError, onCallStateChange, onMuteStateChange, phone, payload);
+}
+
+function hangup(call) {
+    return binding.hangup(call);
+}
+
+function sendDTMF(call, value) {
+    return binding.sendDTMF(call, sendDTMF);
 }
 
 module.exports = {
     initialize,
     getMetaInfo,
     login,
-    makeCall
+    logout,
+    makeCall,
+    hangup,
+    sendDTMF,
 };
